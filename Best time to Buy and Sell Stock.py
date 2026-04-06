ab900434681400ae5397512d5b75266d59634f12
Bharath Kumar Boya <bharathkumar219219@gmail.com>
2026-04-06 10:46:07 +0530

    Add stock profit calculation problem statement
    Added problem statement and initial function definition for calculating stock profit.

--- Best time to Buy and Sell Stock.py
+++ Best time to Buy and Sell Stock.py
@@ -1,3 +1,4 @@
+"""
 Q. You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
@@ -5,6 +6,7 @@ You want to maximize your profit by choosing a single day to buy one stock and c
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
 SOLUTION:
+"""
 
 def profit(prices):
     if not prices or len(prices)==1:
