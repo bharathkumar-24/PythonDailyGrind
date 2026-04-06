ca29838de625b0e0efce0e68a06f3fc78f7cfd17
Bharath Kumar Boya <bharathkumar219219@gmail.com>
2026-04-06 10:46:38 +0530

    Implement prefix check function in Python
    Added a function to check if a word is a prefix of any word in a sentence.

--- Check If a Word Occurs As a Prefix of Any Word in a Sentence.py
+++ Check If a Word Occurs As a Prefix of Any Word in a Sentence.py
@@ -1,3 +1,4 @@
+"""
 Q. Given a sentence that consists of some words separated by a single space, and a searchWord, check if searchWord is a prefix of any word in sentence.
 
 Return the index of the word in sentence (1-indexed) where searchWord is a prefix of this word. If searchWord is a prefix of more than one word, return the index of the first word (minimum index). If there is no such word return -1.
@@ -21,6 +22,7 @@ Output: -1
 Explanation: "you" is not a prefix of any word in the sentence.
 
 SOLUTION:
+"""
 
 def isPrefixOfWord(sentence, searchWord):
     words=sentence.split()
