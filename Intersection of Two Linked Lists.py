1c5b317dd562b8783f0e939e7c38398c59723030
Bharath Kumar Boya <bharathkumar219219@gmail.com>
2026-04-07 10:52:51 +0530

    Update and rename Intersection of Two Linked Lists to Intersection of Two Linked Lists.py

--- Intersection of Two Linked Lists.py
+++ Intersection of Two Linked Lists.py
@@ -1,3 +1,4 @@
+"""
 Q. Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
 
 For example, the following two linked lists begin to intersect at node c1:
@@ -25,6 +26,7 @@ Output: Intersected at '8'
 Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
 
 SOLUTION:
+"""
 
 # Definition for singly-linked list.
 # class ListNode(object):
@@ -46,7 +48,7 @@ class Solution(object):
             second=second.next
         return None
             
-
+"""
 From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; 
 There are 3 nodes before the intersected node in B.
 - Note that the intersected node's value is not 1 because the nodes with value 1 in A and B (2nd node in A and 3rd node in B) are different node references.
@@ -68,3 +70,4 @@ Output: No intersection
 Explanation: From the head of A, it reads as [2,6,4]. From the head of B, it reads as [1,5]. Since the two lists do not intersect, intersectVal must be 0, 
 while skipA and skipB can be arbitrary values.
 Explanation: The two lists do not intersect, so return 
+"""
