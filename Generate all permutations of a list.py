73f40885c9ac3cbb2124beacfbda680fdd348496
Bharath Kumar Boya <bharathkumar219219@gmail.com>
2026-04-07 10:46:34 +0530

    Update and rename Generate all permutations of a list to Generate all permutations of a list.py

--- Generate all permutations of a list.py
+++ Generate all permutations of a list.py
@@ -1,7 +1,8 @@
+"""
 Write a Python program to generate all permutations of a list in Python.In mathematics, the notion of permutation relates to the act of arranging all the members of a set into some sequence or order, or if the set is already
 ordered, rearranging (reordering) its elements, a process called permuting.These differ from combinations, which are selections of some members of a set where order is disregarded.
+"""
 
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def permute(input_list):
     if len(input_list) <= 1:
         return [input_list]
