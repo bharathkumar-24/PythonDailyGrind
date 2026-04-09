e285a7c13471bfa0ff40998e7e4f0330ae118429
Bharath Kumar Boya <bharathkumar219219@gmail.com>
2026-04-09 10:12:15 +0530

    Update and rename Longest Harmonious Subsequence to Longest Harmonious Subsequence.py

--- Longest Harmonious Subsequence.py
+++ Longest Harmonious Subsequence.py
@@ -1,3 +1,4 @@
+"""
 We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
 Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
 
@@ -19,6 +20,7 @@ Output: 0
 Explanation: No harmonic subsequence exists.
 
 SOLUTION:
+"""
 
 from collections import Counter
 def findLHS(nums):
