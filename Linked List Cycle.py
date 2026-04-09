4896cb9759a7cf865b1ad28da101823fefa9818f
Bharath Kumar Boya <bharathkumar219219@gmail.com>
2026-04-09 10:11:08 +0530

    Update and rename Linked List Cycle to Linked List Cycle.py

--- Linked List Cycle.py
+++ Linked List Cycle.py
@@ -1,4 +1,5 @@
-Q. Given head, the head of a linked list, determine if the linked list has a cycle in it.
+"""
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
 
 There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 
@@ -23,7 +24,7 @@ Output: false
 Explanation: There is no cycle in the linked list.
 
 SOLUTION:
-
+"""
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
