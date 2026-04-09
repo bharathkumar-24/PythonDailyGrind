f5532f9335d1ef3bfbbd873a896ce3708ff0cb8f
Bharath Kumar Boya <bharathkumar219219@gmail.com>
2026-04-09 10:13:17 +0530

    Update and rename Longest Nice Substring to Longest Nice Substring.py

--- Longest Nice Substring.py
+++ Longest Nice Substring.py
@@ -1,4 +1,5 @@
-Q. A string s is nice if, for every letter of the alphabet that s contains, it appears both in uppercase and lowercase. For example, "abABB" is nice because 'A' and 'a' appear, and 'B' and 'b' appear. However, "abA" is not because 'b' appears, but 'B' does not.
+"""
+A string s is nice if, for every letter of the alphabet that s contains, it appears both in uppercase and lowercase. For example, "abABB" is nice because 'A' and 'a' appear, and 'B' and 'b' appear. However, "abA" is not because 'b' appears, but 'B' does not.
 
 Given a string s, return the longest substring of s that is nice. If there are multiple, return the substring of the earliest occurrence. If there are none, return an empty string.
 
@@ -22,7 +23,7 @@ Output: ""
 Explanation: There are no nice substrings.
 
 SOLUTION:
-
+"""
 def longestNiceSubstring(s):
     n = len(s)
     max_len = 0
