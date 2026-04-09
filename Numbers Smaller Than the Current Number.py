807b227366c2f4f184d1f9699d76b2c579190e6e
Bharath Kumar Boya <bharathkumar219219@gmail.com>
2026-04-09 10:27:24 +0530

    Update and rename Numbers Smaller Than the Current Number to Numbers Smaller Than the Current Number.py

--- Numbers Smaller Than the Current Number.py
+++ Numbers Smaller Than the Current Number.py
@@ -1,4 +1,5 @@
-Q.Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, 
+"""
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, 
 for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
 
 Return the answer in an array.
@@ -22,8 +23,8 @@ Example 3:
 Input: nums = [7,7,7,7]
 Output: [0,0,0,0]
 
-
 SOLUTION:
+"""
 
 def smallerNumbersThanCurrent(nums):
     l=[]
