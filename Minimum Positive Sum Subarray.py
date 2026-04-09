cd7a4b385d1150a0499b8c8dca8f9d8d48f99156
Bharath Kumar Boya <bharathkumar219219@gmail.com>
2026-04-09 10:21:36 +0530

    Update and rename Minimum Positive Sum Subarray to Minimum Positive Sum Subarray.py

--- Minimum Positive Sum Subarray.py
+++ Minimum Positive Sum Subarray.py
@@ -1,3 +1,4 @@
+"""
 You are given an integer array nums and two integers l and r. Your task is to find the minimum sum of a subarray whose size is between l and r (inclusive) and whose sum is greater than 0.
 
 Return the minimum sum of such a subarray. If no such subarray exists, return -1.
@@ -29,6 +30,7 @@ Explanation:
 The subarray [1, 2] has a length of 2 and the minimum sum greater than 0. So, the answer is 3.
 
 SOLUTION:
+"""
 
 def minimumSumSubarray(nums, l, r):
     n = len(nums)
