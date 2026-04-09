effc6ed6f51ec8028ce2681976d17c8f10fd8dc4
Bharath Kumar Boya <bharathkumar219219@gmail.com>
2026-04-09 10:35:51 +0530

    Update and rename Running Sum of 1d Array to Running Sum of 1d Array.py

--- Running Sum of 1d Array.py
+++ Running Sum of 1d Array.py
@@ -1,5 +1,5 @@
-Q. Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
-
+"""
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
 Return the running sum of nums.
 
 Example 1:
@@ -20,7 +20,7 @@ Input: nums = [3,1,2,10,1]
 Output: [3,4,6,16,17]
  
 SOLUTION:
-
+"""
 
 def runningSum(nums):
     for i,val in enumerate(nums):
