59d6467a762acb72ee4ec514515b199a8d4f7730
Bharath Kumar Boya <bharathkumar219219@gmail.com>
2026-04-15 10:00:05 +0530

    Update and rename Spiral Matrix to Spiral Matrix.py

--- Spiral Matrix.py
+++ Spiral Matrix.py
@@ -1,4 +1,5 @@
-Q.  Given an m x n matrix, return all elements of the matrix in spiral order.
+"""
+Given an m x n matrix, return all elements of the matrix in spiral order.
 
 Example 1:
 Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
@@ -9,8 +10,8 @@ Example 2:
 Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
 Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 
-
 SOLUTION:
+"""
 
 def spiral_matrix(matrix):
     ret=[]
